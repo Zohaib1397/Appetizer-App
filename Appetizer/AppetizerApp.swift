780b2c3276483acfb5219fc0c54456e8efb35efd
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2024-07-22 05:58:11 +0500

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Zohaib Ahmed on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
